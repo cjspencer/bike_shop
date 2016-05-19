571ad8ec5c0ce69ea5a6fb1637b68c7a53d9aa1d
cjspencer <chadwick.j.spencer@me.com>
2016-05-18 21:54:29 -0700

    Added some links

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,22 +1,26 @@
 class BikesController < ApplicationController
-  def new
-  end
-
-  def create
-    # render plain: params[:bike].inspect
-    @bike = Bike.new(article_params)
-
-    @bike.save
-    redirect_to @bike
+  def index
+    @bikes = Bike.all
   end
 
   def show
     @bike = Bike.find(params[:id])
   end
 
-private
-  def article_params
-    params.require(:bike).permit(:title, :text)
+  def new
+    @bike = Bike.new
+  end
 
-end
+  def create
+    # render plain: params[:bike].inspect
+    @bike = Bike.new(bike_params)
+    @bike.save
+    redirect_to @bike
+  end
+
+
+private
+  def bike_params
+    params.require(:bike).permit(:title, :text)
+  end
 end
